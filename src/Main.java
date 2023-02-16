07dcc5bba4b6a16af3410d7348156eb3a650c22c
Eвгений <mocard778@gmail.com>
2023-02-16 13:56:13 +0700

    3 задача

---
+++ src/Main.java
@@ -0,0 +1,65 @@
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+        task1();
+        task2();
+
+    }
+
+    public static void task1() {
+        System.out.println("Задача 1 2");
+        int[] number = new int[3];
+        number[0] = 1;
+        number[1] = 2;
+        number[2] = 3;
+        for (int i = 0; i < 3; i++) {
+            System.out.print(number[i]);
+            if (i != number.length - 1)
+                System.out.print(", ");
+        }
+        System.out.println();
+        double[] drob = new double[]{1.57, 7.654, 9.986};
+        for (int j = 0; j < 3; j++) {
+            System.out.print(drob[j]);
+            if (j != drob.length - 1)
+                System.out.print(", ");
+        }
+        System.out.println();
+        int[] proizvol = new int[]{2, 4, 6, 8, 10};
+        for (int i = 0; i < proizvol.length; i++) {
+            System.out.print(proizvol[i]);
+            if (i != proizvol.length - 1)
+                System.out.print(", ");
+        }
+        System.out.println();
+        System.out.println();
+    }
+
+    public static void task2() {
+        System.out.println("Задача 3");
+        int[] number = new int[3];
+        number[0] = 1;
+        number[1] = 2;
+        number[2] = 3;
+            for (int i = number.length - 1;i >= 0; i--){
+                System.out.print(number[i]);
+                if (i != 0)
+                    System.out.print(", ");
+        }
+        System.out.println();
+        double[] drob = new double[]{1.57, 7.654, 9.986};
+        for (int i = drob.length - 1; i >= 0; i--){
+            System.out.print(drob[i]);
+            if (i != 0)
+                System.out.print(", ");
+        }
+        System.out.println();
+         int[] proizvol = new int[]{2, 4, 6, 8, 10};
+         for ( int i = proizvol.length - 1; i >= 0; i--)
+        {System.out.print(proizvol[i]);
+         if (i != 0)
+             System.out.print(", ");}
+    }
+
+}
